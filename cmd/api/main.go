e3f403e329f36831a13cf4319f95f67f50ffa10b
Fernando Tomio <nandotomio@gmail.com>
2022-09-14 17:21:27 -0300

    feat: add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	server := fiber.New()
+
+	postgresDns := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"localhost",
+		"dev",
+		"change_please",
+		"dev",
+		"5432",
+	)
+	postgresDialector := postgres.Open(postgresDns)
+	_, err := gorm.Open(postgresDialector, &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("DB connection error. Error: %v", err.Error())
+	}
 
 	server.Get("/health", func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).JSON("pong :)")
